74395fdb9f5b56fa55b7640446b6e9f3ed807e2d
kwamena-koomson <kwamenakooms@gmail.com>
2024-05-18 23:13:11 +0100

    pool errors fixed

--- database/index.js
+++ database/index.js
@@ -1,37 +1,41 @@
-const { Pool } = require("pg")
-require("dotenv").config()
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
+const { Pool } = require("pg");
+require("dotenv").config();
+
+let pool;
+
+// Validate NODE_ENV and set SSL options accordingly
+if (process.env.NODE_ENV === "development") {
+  // Development environment
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // Accept self-signed certificates
     },
-})
-
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
+  });
 } else {
+  // Production or other environments
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
+    // Add SSL configurations for production environment if needed
+  });
 }
+
+/**
+ * Executes an SQL query using the connection pool.
+ * The first parameter is the SQL query string.
+ * The second parameter is an array of optional parameters for the query.
+ * Returns a Promise that resolves with the query result.
+ */
+async function query(text, params) {
+  try {
+    const res = await pool.query(text, params);
+    console.log("Executed query:", { text });
+    return res;
+  } catch (error) {
+    console.error("Error in query:", { text });
+    throw error;
+  }
+}
+
+// Export the query function
+module.exports = { query };
